ea5b115238c27bd2c77fcd6243eb5a0e83c244e4
KodyJKing <kody.j.king@gmail.com>
2026-08-20 19:52:44 -0700

    Use Spark's new axis binding API for remaining hard coded Mario inputs.

--- smc64/src/mods/mario/MarioInput.cpp
+++ smc64/src/mods/mario/MarioInput.cpp
@@ -1,52 +1,40 @@
 #include "MarioInput.hpp"
 #include "spark/input/Bindings.hpp"
-#include <Xinput.h>
 #define DIRECTINPUT_VERSION 0x0800
 #include <dinput.h>
-#pragma comment( lib, "Xinput.lib" )
-
-#include "engine/halo1.hpp"
-#include "Coordinates.hpp"
 
 namespace Mod::Mario {
 
     void registerInputActions() {
-        const Spark::Input::ButtonCode kCrouch[]     = { SPARK_GAMEPAD_LEFT_SHOULDER, DIK_LSHIFT };
-        Spark::Input::addAction("mario:crouch", kCrouch, 2);
-        const Spark::Input::ButtonCode kBulletTime[] = { SPARK_GAMEPAD_DPAD_UP, SPARK_GAMEPAD_LEFT_THUMB, SPARK_GAMEPAD_RIGHT_SHOULDER, DIK_LCONTROL };
-        Spark::Input::addAction("mario:bullet_time", kBulletTime, 4);
-    }
-
-    void updateXboxControls(SM64MarioInputs& inputs) {
-        XINPUT_STATE state = {0};
-        XInputGetState(0, &state);
-
-        inputs.buttonZ |= (Spark::Input::actionState("mario:crouch") & 0x80) ? 1 : 0;
-    }
-
-    void updateKeyboardControls(SM64MarioInputs& inputs) {
-        // Buttons
-        inputs.buttonZ |= (Spark::Input::actionState("mario:crouch") & 0x80) ? 1 : 0;
-    }
-
-    void updateMappedHaloControls(SM64MarioInputs& inputs) {
-        auto playerController = Engine::getPlayerControllerPointer();
-        if (playerController) {
-            bool cheifMelee = (playerController->actions & Engine::PlayerActionFlags::melee) != 0;
-            bool cheifJump = (playerController->actions & Engine::PlayerActionFlags::jump) != 0;
-            if (cheifMelee) inputs.buttonB = 1;
-            if (cheifJump) inputs.buttonA = 1;
-            inputs.stickX = -playerController->walkX;
-            inputs.stickY = playerController->walkY;
-        }
+        Spark::Input::addAction("mario:crouch", { SPARK_GAMEPAD_LEFT_SHOULDER, DIK_LSHIFT });
+        Spark::Input::addAction("mario:bullet_time", { SPARK_GAMEPAD_DPAD_UP, SPARK_GAMEPAD_LEFT_THUMB, SPARK_GAMEPAD_RIGHT_SHOULDER, DIK_LCONTROL });
+        Spark::Input::addAction("mario:jump",       {SPARK_GAMEPAD_A, DIK_SPACE });
+        Spark::Input::addAction("mario:punch",      {SPARK_GAMEPAD_B, DIK_F});
+        Spark::Input::addAction("mario:walk_right", {SPARK_GAMEPAD_LEFT_STICK_RIGHT, DIK_D});
+        Spark::Input::addAction("mario:walk_left",  {SPARK_GAMEPAD_LEFT_STICK_LEFT, DIK_A});
+        Spark::Input::addAction("mario:walk_fwd",   {SPARK_GAMEPAD_LEFT_STICK_UP, DIK_W});
+        Spark::Input::addAction("mario:walk_back",  {SPARK_GAMEPAD_LEFT_STICK_DOWN, DIK_S});
     }
 
     void updateInput(SM64MarioInputs& inputs, SM64MarioState& marioState, Engine::Camera* camera) {
         inputs = {};
-        updateXboxControls(inputs);
-        updateKeyboardControls(inputs);
-        updateMappedHaloControls(inputs);
-        
+        inputs.buttonZ = (Spark::Input::actionState("mario:crouch") & 0x80) ? 1 : 0;
+        inputs.buttonA = (Spark::Input::actionState("mario:jump")   & 0x80) ? 1 : 0;
+        inputs.buttonB = (Spark::Input::actionState("mario:punch")  & 0x80) ? 1 : 0;
+
+        inputs.stickX  = Spark::Input::actionAxis("mario:walk_right") - Spark::Input::actionAxis("mario:walk_left");
+        inputs.stickY  = Spark::Input::actionAxis("mario:walk_fwd")   - Spark::Input::actionAxis("mario:walk_back");
+
+        const float deadZone = 0.05f;
+        if (fabs(inputs.stickX) < deadZone) inputs.stickX = 0.0f;
+        if (fabs(inputs.stickY) < deadZone) inputs.stickY = 0.0f;
+
+        float length = sqrt(inputs.stickX * inputs.stickX + inputs.stickY * inputs.stickY);
+        if (length > 1.0f) {
+            inputs.stickX /= length;
+            inputs.stickY /= length;
+        }
+
         if (camera) {
             inputs.camLookX = -camera->fwd.x;
             inputs.camLookZ = -camera->fwd.y;
@@ -54,3 +42,4 @@ namespace Mod::Mario {
     }
 
 }
+
